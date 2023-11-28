c328b2a7f051b8477c52903abd8871a5190b6bd8
Prokop <prokop.schield@protonmail.com>
2023-11-28 16:04:19 +0100

    Release: v0.1.0-2

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,62 @@
 import { encode, decode } from "doge-don";
 import { cacheFn } from "ps-std";
-import { Writable, writable } from "svelte/store";
 
-export const store = cacheFn(<T>(key: string) => {
+export const noop = () => {};
+
+export interface Store<T> {
+	value: T;
+	set(value: T): void;
+	update(_callback: (_value: T) => T): void;
+	subscribe(_callback: (_value: T) => void): () => void;
+}
+
+export const store = cacheFn(<T>(key: string): Store<T> => {
 	let value: T = decode(localStorage.getItem(key) || "");
-	const store = writable<T>(value);
 
-	store.subscribe((new_value: T) => {
-		localStorage.setItem(key, encode((value = new_value)));
-	});
+	const subscribers = new Set<(_value: T) => void>();
 
-	return {
-		...store,
+	const set = (new_value: T) => {
+		if (value !== new_value) {
+			localStorage.setItem(key, encode((value = new_value)));
+
+			for (const subscriber of subscribers) {
+				Promise.resolve(subscriber(new_value)).catch(noop);
+			}
+		}
+	};
+
+	const subscribe = (callback: (_value: T) => void) => {
+		subscribers.add(callback);
+
+		return () => subscribers.delete(callback);
+	};
+
+	const update = (callback: (_value: T) => T) => {
+		set(callback(value));
+	};
+
+	const store = {
 		get value() {
 			return value;
 		},
 		set value(new_value) {
 			store.set(new_value);
 		},
+		set,
+		subscribe,
+		update,
 	};
+
+	return store;
 });
 
-export const state = new Proxy(
-	{} as Record<string, Writable<any> & { value: any }>,
-	{
-		get(_target, key) {
-			return store(String(key));
-		},
-		set(_target, key, value) {
-			store(String(key)).set(value);
+export const state = new Proxy({} as Record<string, Store<any>>, {
+	get(_target, key) {
+		return store(String(key));
+	},
+	set(_target, key, value) {
+		store(String(key)).set(value);
 
-			return true;
-		},
-	}
-);
+		return true;
+	},
+});
